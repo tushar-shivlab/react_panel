c6dff1001a95f0033ce8043f446a4ff7f25fba12
tushar-shivlab <80961599+tushar-shivlab@users.noreply.github.com>
2021-06-22 18:36:32 +0530

    commit from direct

---
+++ src/components/Users/index.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react';
+import Profile from '../../assets/image/download.jpeg';
+
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
+
+const groupId = cookies.get('groupId');
+
+function UserList({ group, socket }) {
+    const [usergroup, setusergroup] = useState([]);
+
+    useEffect(() => {
+        setusergroup(group);
+    }, [group]);
+
+    useEffect(() => {
+        // const newarr = [];
+        socket.on(groupId, function (data) {
+            console.log('data :>> ', data);
+            // newarr.push(data);
+            // console.log('newarr :>> ', newarr);
+            setusergroup((prev) => ({ ...prev, data }));
+        });
+        return () => {
+            socket.off(groupId);
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    let i = 1;
+    console.log('usergroup :>> ', usergroup);
+
+    return (
+        <ul className="list bg-gray-500">
+            {usergroup?.length &&
+                usergroup?.map((users) => {
+                    return (
+                        <li
+                            key={users._id}
+                            className="inline-block shadow-lg p-3 m-2 bg-green-300">
+                            <div className="w-12 float-left text-center p-2">
+                                {i++}
+                            </div>
+                            <div className="w-25 float-left pl-5 w-20 opacity-1">
+                                <p>{users.name}</p>
+                                <p>Point 0</p>
+                            </div>
+                            <div className="w-18 float-left pl-5">
+                                <img
+                                    className="h-12 float-left"
+                                    src={Profile}
+                                    alt="demo"
+                                />
+                            </div>
+                        </li>
+                    );
+                })}
+        </ul>
+    );
+}
+export default UserList;
---
+++ src/constant/common.js
@@ -0,0 +1,7 @@
+let common= {
+    socket_base_url:'http://127.0.0.1:3010',
+    common_url:'http://localhost:3010/api/v1/',
+    group_join:'user/group/join',
+    group_get:'group/',
+}
+export default common
